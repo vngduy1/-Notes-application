ba751422d8e5e8f5cb7b383b1295b5f67644d8e8
Duy <duyvu372001@gmail.com>
2024-05-02 14:09:40 +0900

    connectDatabase

---
+++ client/src/utils/constain.js
@@ -0,0 +1,1 @@
+export const GRAPHQL = 'http://localhost:4000';
--- server/server.mjs
+++ server/server.mjs
@@ -5,60 +5,18 @@ import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHt
 import bodyParser from "body-parser";
 import { expressMiddleware } from "@apollo/server/express4";
 import cors from "cors";
-import fakeData from "./fakeData/index.js";
+import dotenv from "dotenv";
+import { resolvers } from "./resolvers/index.js";
+import { typeDefs } from "./schema/index.js";
+dotenv.config();
+import mongoose from "mongoose";
+import "./firebaseConfig.js";
 
 const app = express();
 const httpServer = http.createServer(app);
 
-const typeDefs = `#graphql
-type Folder {
-  id:String,
-  name: String,
-  createAt: String
-  author: Author
-  notes: [Note]
-}
-
-type Note  {
-  id: String,
-  content: String
-}
-
-type Author {
-  id: String,
-  name: String,
-}
-
-type Query {
-  folders: [Folder],
-  folder(folderId: String): Folder,
-  note(noteId:String): Note
-}
-`;
-const resolvers = {
-  Query: {
-    folders: () => {
-      return fakeData.folders;
-    },
-    folder: (parent, args) => {
-      const folderId = args.folderId;
-      return fakeData.folders.find((folder) => folder.id === folderId);
-    },
-    note: (parent, args) => {
-      const noteId = args.noteId;
-      return fakeData.notes.find((note) => note.id === noteId);
-    },
-  },
-  Folder: {
-    author: (parent, args) => {
-      const authorId = parent.authorId;
-      return fakeData.authors.find((author) => author.id === authorId);
-    },
-    notes: (parent, args) => {
-      return fakeData.notes.filter((note) => note.folderId === parent.id);
-    },
-  },
-};
+const URL = process.env.MONGO;
+const PORT = process.env.PORT || 4000;
 
 const server = new ApolloServer({
   typeDefs,
@@ -71,8 +29,14 @@ await server.start();
 //middleware
 app.use(cors(), bodyParser.json(), expressMiddleware(server));
 
-await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve))
-  .then(() => {
-    console.log("Server ready at http://localhost:4000");
+mongoose
+  .connect(URL)
+  .then(async () => {
+    console.log("connect successfully");
+    await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve))
+      .then(() => {
+        console.log("Server ready at http://localhost:4000");
+      })
+      .catch((e) => console.log(e));
   })
-  .catch((e) => console.log(e));
+  .catch((e) => console.log("connect fail from", e.message));
